909a017ab2d61497f0e022161b9f6366840da9b7
67uwu <vlink314@gmail.com>
2026-02-15 10:59:52 -0800

    stuff

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -24,8 +24,7 @@ public class Robot extends TimedRobot {
    */
   public Robot() {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
-    // autonomous chooser on the dashboard.
-    m_robotContainer = new RobotContainer();
+    // autonomous chooser on the dashboard
   }
 
   /**
@@ -54,7 +53,7 @@ public class Robot extends TimedRobot {
   /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
   @Override
   public void autonomousInit() {
-    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
+    
 
     // schedule the autonomous command (example)
     if (m_autonomousCommand != null) {
